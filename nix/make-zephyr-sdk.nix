6c6b280f7beb906256b3b92b35b6d97e62035b36
Jimmy Brisson <theotherjimmy@gmail.com>
2022-10-31 13:31:54 -0500

    Update zephyr sdk to 0.15.0

--- nix/make-zephyr-sdk.nix
+++ nix/make-zephyr-sdk.nix
@@ -1,22 +1,20 @@
-{ stdenv, fetchurl, which, python38, autoPatchelfHook }: arch: hash:
+{ stdenv, fetchurl, which, python38, autoPatchelfHook, lib }:
 let
-  version = "0.13.2";
+  version = "0.15.0";
 in
 stdenv.mkDerivation {
   name = "zephyr-sdk";
   inherit version;
   src = fetchurl {
-    url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/zephyr-toolchain-${arch}-${version}-linux-x86_64-setup.run";
-    inherit hash;
+    url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/zephyr-sdk-${version}_linux-x86_64.tar.gz";
+    hash = "sha256-mTwj3BBLfm4hGxow7D27+T9+Q10eEDtjyTxuM/M/QV8=";
   };
   nativeBuildInputs = [
-    which
-    python38
     autoPatchelfHook
   ];
-  # We can't do this in one  step, because the .run script
-  # expects /bin/bash to exist. Instead, we break this up and this
-  # seems to work fine.
-  unpackPhase = "sh $src --noexec --target .";
-  installPhase = "sh setup.sh -d $out -norc -nocmake -y";
+  buildInputs = [
+    stdenv.cc.cc.lib
+    python38
+  ];
+  installPhase = "mkdir $out; cp -r . $out";
 }
